d622a9a58ef31de98272c04dfd8d17750c6c04e1
francizjrr <franciscoguez03@gmail.com>
2024-09-04 17:25:55 -0600

    Creación de Carpeta dedicada para el contenido de las clases diarias de JAVASCRIPT de  Kodigo

---
+++ M2_JS/Contenido_JS/main.js
@@ -0,0 +1,70 @@
+
+//{}
+//""
+
+// COMENTAR EN UNA LINEA -> DESACTIVAR UNA LINEA PARA EL ENTORNO DE EJECUCION
+/* 
+    COMENTARIO
+    MULTI
+    LINEAS
+*/
+
+
+// El concepto del lenguaje
+/* Caracteristicas:
+* Tipo de tipado: Debilmente Tipado -> LA FORMA DE ESCRIBIR (Nos perdona errores)
+* Tipado debil NO NECESITA:
+- CIERRES INSTRUCCION ;
+- ACLARAR EL TIPO DE DATO DE LAS VARIABLES 
+* PARADIGMA DE PROGRAMACION -> FORMA DE ESCRIBIR EL CODIGO: Orientado a objetos (prototipos)
+* FORMA DE ESCRIBIR CODIGO ES PROGRAMACION ESTRUCTURADA (FUNCIONES)
+* ENTORNO DE EJECUCION: Interpretado (Ejecutado en el navegador)
+*/
+
+
+// Impresion 
+console.log(1);
+//console.log(2);
+
+
+//Tipos de datos
+//Number -> Numero (enteros o decimales)
+console.log(1);
+
+//String -> Cadena de texto
+console.log("Hola");
+
+//Boolean -> Boleano VERDADERO O FALSO
+console.log(true);
+
+
+//Declaracion de variables
+//Variables -> Un espacio en memoria, que su valor es mutable
+/*VAR
+    - SON REDECLARABLES
+    - Scope -> ALCANCE
+*/
+var nombreVariable = false;
+var nombre = "Edwin";
+var nombre = "Jairo"
+console.log(nombre);
+
+
+/* LET
+    -NO son redeclarables
+    - Scope
+*/
+
+let numero = 1;
+console.log(numero);
+numero = 3;
+console.log(numero);
+
+/* CONSTANTES
+    - NO SON REDECLARABLES
+    - NO SON MUTABLES
+*/
+// ES ALGO NO MUTABLE EN TIEMPO DE EJECUCION
+//Declaracion de constante
+const pi = 3.1416;
+console.log(pi);
