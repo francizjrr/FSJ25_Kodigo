0aa0ac04c402b2044500f34f587551aeb9519a92
francizjrr <franciscoguez03@gmail.com>
2024-11-13 14:04:50 -0600

    Cotenido PHP

--- M4_PHP/ELED.php
+++ M4_PHP/ELED.php
@@ -1,7 +1,62 @@
 <?php
 
+//* Ejercicio 1: Lista Invertida
+function invertirLista($array) {
+    return array_reverse($array);
+}
 
+$array = [1, 2, 3, 4, 5];
+$resultado = invertirLista($array);
+print_r($resultado);
 
+//* Ejercicio 2: Numeros pares
+function sumaNumerosPares($array) {
+    $suma = 0;
+    foreach ($array as $num) {
+        if ($num % 2 == 0) {
+            $suma += $num;
+        }
+    }
+    return $suma;
+}
 
+$array = [1, 2, 3, 4, 5, 6];
+echo "Suma de números pares: " . sumaNumerosPares($array);
+
+//* Ejercicio 3: Frecuencia
+function frecuenciaCaracteres($cadena) {
+    $frecuencia = [];
+    for ($i = 0; $i < strlen($cadena); $i++) {
+        $caracter = $cadena[$i];
+        if (isset($frecuencia[$caracter])) {
+            $frecuencia[$caracter]++;
+        } else {
+            $frecuencia[$caracter] = 1;
+        }
+    }
+    return $frecuencia;
+}
+
+$cadena = "hola mundo";
+$resultado = frecuenciaCaracteres($cadena);
+print_r($resultado);
+
+//* Ejercicio 4:  Bucle Anidado
+function imprimirPiramide($altura) {
+    for ($i = 1; $i <= $altura; $i++) {
+        // Imprimir espacios en blanco
+        for ($j = $altura - $i; $j > 0; $j--) {
+            echo " ";
+        }
+        // Imprimir asteriscos
+        for ($k = 1; $k <= (2 * $i - 1); $k++) {
+            echo "*";
+        }
+        echo "\n";
+    }
+}
+
+$altura = 5;
+imprimirPiramide($altura);
 
 ?>
--- M4_PHP/estructuras-datos.php
+++ M4_PHP/estructuras-datos.php
@@ -1,8 +1,7 @@
+<?php 
 
-<?php
-
-//*TEMA: Estructuras de datos
-//? ARRAYS
+//Estructuras de datos
+//Arrays
 
 //Declaraciones de arrays
 //Inicializacion a traves de constructor de objeto
@@ -15,41 +14,41 @@ $array = [];
 //$array = array();
 
 
-//? METODOS DE ARRAY
-//! ---  Ingresar datos a un array ---
+// METODOS DE ARRAY
+//---  Ingresar datos a un array ---
 // EN LA ULTIMA POSICION
 array_push($array, 1);
 
 // EN LA PRIMER POSICION
-array_unshift($array, 5);
+array_unshift($array,5);
 
-//! --- ELIMINAR datos a un array ---
+// --- ELIMINAR datos a un array ---
 // PRIMER POSICION
 array_shift($array);
 
 // ULTIMA POSICION
 array_pop($array);
 
-//! --- Imprimir un array ---
+// --- Imprimir un array ---
 print_r($array);
 
-// CALCULAR EL LARGOR O LONGANISMO DE UN ARRAY
+//CALCULAR EL LARGOR O LONGANISMO DE UN ARRAY
 $lenght = count($array);
 
 //Prueba
 $arraycito = [];
-array_push($arraycito, "a");
-array_push($arraycito, "b");
-array_push($arraycito, "c");
-array_unshift($arraycito, "z");
+array_push($arraycito,"a");
+array_push($arraycito,"b");
+array_push($arraycito,"c");
+array_unshift($arraycito,"z");
 array_shift($arraycito);
 array_pop($arraycito);
 //Ingresar datos en la ultima posicion sin el array_push
 $arraycito[] = "d";
 print_r($arraycito);
 
-//*TEMA: ARRAYS ASOCIATIVOS
-$arrayzote = ["nombre" => "Jairo", "apellido" => "Vega Romero"];
+//ARRAYS ASOCIATIVOS
+$arrayzote = [ "nombre" => "Jairo", "apellido" => "Vega Romero"];
 print_r($arrayzote);
 print_r(array_key_last($arrayzote));
 echo "\n";
@@ -57,57 +56,52 @@ print_r($arrayzote[array_key_last($arrayzote)]);
 print(count($arrayzote));
 
 
-//*TEMA: CLASES Y OBJETOS
+// CLASES Y OBJETOS
 
-class Persona
+class Persona { 
+
+//Propiedades o atributos
+public $nombre;
+public $edad;
+
+//Constructor 
+function __construct($nombreParam, $edadParam)
 {
-
-    //Propiedades o atributos
-    public $nombre;
-    public $edad;
-
-    //Constructor 
-    function __construct($nombreParam, $edadParam)
-    {
-        $this->nombre = $nombreParam;
-        $this->edad = $edadParam;
-    }
-
-    //Metodos
-    function respirar()
-    {
-        return "Estoy respirando";
-    }
+$this->nombre = $nombreParam;
+$this->edad = $edadParam;
 }
 
-$personita = new Persona("Jairo", 75);
+//Metodos
+function respirar(){
+return "Estoy respirando";
+}
+}
 
-print($personita->nombre . '\n');
+$personita = new Persona("Jairo",75);
+
+print($personita->nombre.'\n');
 print($personita->respirar());
 
 print_r($personita);
 
 
-//*TEMA: Stack (pila) -> LIFO LastInFirstOut
+//Stack (pila) -> LIFO LastInFirstOut
 
-class Stack
+class Stack{
+public $stack;
+
+function __construct()
 {
-    public $stack;
+$this->stack = array();
+}
 
-    function __construct()
-    {
-        $this->stack = array();
-    }
+function add($value){
+array_push($this->stack, $value);
+}
 
-    function add($value)
-    {
-        array_push($this->stack, $value);
-    }
-
-    function delete()
-    {
-        array_pop($this->stack);
-    }
+function delete(){
+array_pop($this->stack);
+}
 }
 
 $pilaRopa = new Stack();
@@ -117,26 +111,23 @@ $pilaRopa->add("Pantalon");
 $pilaRopa->delete();
 print_r($pilaRopa);
 
-//*TEMA: Queue o Colas -> FIFO -> FirstInFirstOut
+// Queue o Colas -> FIFO -> FirstInFirstOut
 
-class Queue
+class Queue { 
+public $queue;
+
+function __construct()
 {
-    public $queue;
+$this->queue = array();
+}
 
-    function __construct()
-    {
-        $this->queue = array();
-    }
+function enqueue($value){
+array_push($this->queue,$value);
+}
 
-    function enqueue($value)
-    {
-        array_push($this->queue, $value);
-    }
-
-    function dequeue()
-    {
-        return array_shift($this->queue);
-    }
+function dequeue(){
+return array_shift($this->queue);
+}
 }
 
 $caseta1 = new Queue();
@@ -147,54 +138,91 @@ print("Dato eliminado: {$caseta1->dequeue()} \n");
 
 print_r($caseta1);
 
-class Node
-{
-    public $value;
-    public $next;
+class Node{
+public $value;
+public $next;
 
-    function __construct($data)
-    {
-        if ($data === null) {
-            $this->value = 0;
-            $this->next = null;
-        } else {
-            $this->value = $data;
-            $this->next = null;
-        }
-    }
+function __construct($data)
+{
+if($data === null ){
+$this->value = 0;
+$this->next = null;
+}else{
+$this->value = $data;
+$this->next = null;
+}
+}
 }
 
-class LinkedList
+class LinkedList{
+public $head;
+
+function __construct()
 {
-    public $head;
+$this->head = null;
+}
 
-    function __construct()
-    {
-        $this->head = null;
-    }
+function insert($data){
+$newNode = new Node($data);
 
-    function insert($data)
-    {
-        $newNode = new Node($data);
+if($this->head === null){
+$this->head = $newNode;
+}else{
+//Variable auxiliar
+$aux = $this->head;
 
-        if ($this->head === null) {
-            $this->head = $newNode;
-        } else {
-            //Variable auxiliar
+while($aux->next !== null){
+$aux = $aux->next;
+}
+
+$aux->next = $newNode;
+}
+}
+
+        function delete($data){
+            if($this->head === null){
+                return "La lista estaba previamente vacia. \n";
+            }
+
+            if($this->head->value === $data){
+                print("Se Encontro. \n");
+                $this->head = $this->head->next;
+                return "Encontrado";
+            }
+            
+            
             $aux = $this->head;
 
-            while ($aux->next !== null) {
+
+            while($aux->next !== null ){
+               if($aux->next->value == $data){
+                $aux->next = $aux->next->next;
+                print("Si se elimino. \n");
+                return "Se ha eliminado el dato.";
+               }
+
                 $aux = $aux->next;
             }
 
-            $aux->next = $newNode;
+            return "Ese dato no existe en la lista.";
+
         }
-    }
+
+        // Generar un metodo el cual me imprima o retorne el length de la lista
+        
+
+        // Generar un metodo para vaciar la lista
+
 }
 
 $listita = new LinkedList();
 $listita->insert(3);
 $listita->insert(5);
 $listita->insert(100);
+    $listita->delete(5);
 print_r($listita);
+
+
+
+
 ?> 
